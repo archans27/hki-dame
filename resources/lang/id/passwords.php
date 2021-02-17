d444256ba82ec6115264a5f3651ed61471d5a51a
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-02-17 20:01:42 +0700

    Add indonesia lang resource

---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'reset' => 'Password anda sudah direset!',
+    'sent' => 'Kami telah mengirim link perubahan password!',
+    'throttled' => 'Mohon menunggu sebelum mencoba kembali.',
+    'token' => 'Token password reset ini tidak falid.',
+    'user' => "Kami tidak dapat menemukan user dengan email yang diberikan.",
+
+];
