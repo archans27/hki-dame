dc60bc3b2683786341986dac64fcfb414d335e04
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-02-17 23:12:32 +0700

    navigation menu styling

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,9 +33,9 @@
         </button>
         <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
           <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-            
             <x-responsive-nav-link class="active" :active="0" href="{{url('/jemaat')}}">Jemaat</x-responsive-nav-link>
-           
+            <x-responsive-nav-link class="active" :active="0" href="{{url('/keluarga')}}">Keluarga</x-responsive-nav-link>
+            <x-responsive-nav-link class="active" :active="0" href="{{url('/sektor')}}">Sektor</x-responsive-nav-link>
           </div>
         </div>
       </div>
