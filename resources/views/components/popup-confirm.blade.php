a8d01fb107892dc794a4d95b8bfa4eccaa7556fa
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-02-17 20:23:48 +0700

    break popup-confirm into new component

---
+++ resources/views/components/popup-confirm.blade.php
@@ -0,0 +1,39 @@
+@props(['title', 'message', 'action', 'actionUrl', 'actionMethod' => 'GET' ])
+
+<div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="modal-id">
+    <div class="relative w-auto my-6 mx-auto max-w-3xl">
+      <!--content-->
+      <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+        <!--header-->
+        <div class="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+          <h3 class="text-3xl font-semibold text-black">
+            {{$title}}
+          </h3>
+          <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onclick="toggleModal('modal-id')">
+            <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+              ×
+            </span>
+          </button>
+        </div>
+        <!--body-->
+        <div class="relative p-6 flex-auto">
+          <p class="my-4 text-gray-600 text-lg leading-relaxed">
+            {{$message}}
+          </p>
+        </div>
+        <!--footer-->
+        <div class="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
+          <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" onclick="toggleModal('modal-id')">
+            Tutup
+          </button>
+          <form method="POST" action="{{ $actionUrl }}">
+            @method($actionMethod)
+            @csrf
+            <button type="submit" class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" onclick="toggleModal('modal-id')">
+              {{$action}}
+            </button>
+        </form>
+        </div>
+      </div>
+    </div>
+  </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,45 +21,7 @@
                     power_settings_new
                 </span>
               </button>
-        </form>
-
-
-          <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="modal-id">
-            <div class="relative w-auto my-6 mx-auto max-w-3xl">
-              <!--content-->
-              <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <!--header-->
-                <div class="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-                  <h3 class="text-3xl font-semibold text-black">
-                    Logout
-                  </h3>
-                  <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onclick="toggleModal('modal-id')">
-                    <span class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                <!--body-->
-                <div class="relative p-6 flex-auto">
-                  <p class="my-4 text-gray-600 text-lg leading-relaxed">
-                    Apakah anda yakin ingin keluar dari aplikasi?
-                  </p>
-                </div>
-                <!--footer-->
-                <div class="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
-                  <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" onclick="toggleModal('modal-id')">
-                    Tutup
-                  </button>
-                  <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" onclick="toggleModal('modal-id')">
-                      Keluar Dari Aplikasi
-                    </button>
-                </form>
-                </div>
-              </div>
-            </div>
-          </div>
+        </form>          
           <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="modal-id-backdrop"></div>
           <script type="text/javascript">
             function toggleModal(modalID){
@@ -86,4 +48,11 @@
         </div>
       </div>
     </nav>
+    <x-popup-confirm 
+      :title="'LOGOUT'"
+      :message="'Apakah anda yakin akan keluar dari aplikasi?'"
+      :action="'Keluar dari aplikasi'"
+      :actionUrl="url('logout')"
+      :actionMethod="'POST'"
+    />
   </div>
