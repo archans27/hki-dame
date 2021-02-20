fa20fbdf1191ced973567e958365f079f96402ce
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-02-20 17:15:20 +0700

    fix navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,7 +14,7 @@
       <hr/> 
       <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <button type="submit" class="active w-full text-left my-3"
+        <button type="submit" class="w-full text-left my-3"
                 onclick="event.preventDefault();
                     toggleModal('modal-logout');">
             HKI Admin
@@ -25,20 +25,59 @@
         </form>
       <hr class="mb-5"/>
       
-      <x-responsive-nav-link class="active" :active="1" href="{{url('/dashboard')}}">Dashboard</x-responsive-nav-link>
-      <div @click.away="open = false" class="relative" x-data="{ open: false }">
+      <div class="relative">
+        <a href="dashboard">
+          <button class="text-right flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-gray-500 text-white rounded-md dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+            <span class="float-left font-bold text-lg">Dashboard</span>
+          </button>
+        </a>
+      </div>
+
+      <div class="relative" x-data="{ open: true }">
         <button @click="open = !open" class="text-right flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-gray-500 text-white rounded-md dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-          <span class="float-left">Data Master</span>
+          <span class="float-left font-bold text-lg">Data Master</span>
           <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
-        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
+        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="w-full mt-2 origin-top-right rounded-md shadow-lg">
           <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-            <x-responsive-nav-link class="active" :active="0" href="{{url('/jemaat')}}">Jemaat</x-responsive-nav-link>
-            <x-responsive-nav-link class="active" :active="0" href="{{url('/keluarga')}}">Keluarga</x-responsive-nav-link>
-            <x-responsive-nav-link class="active" :active="0" href="{{url('/sektor')}}">Sektor</x-responsive-nav-link>
+            <x-responsive-nav-link class="{{ (request()->segment(1) == 'keluarga') ? 'active' : '' }}" :active="0" href="{{url('/keluarga')}}">Keluarga</x-responsive-nav-link>
+            <x-responsive-nav-link class="{{ (request()->segment(1) == 'jemaat') ? 'active' : '' }}" :active="0" href="{{url('/jemaat')}}">Jemaat</x-responsive-nav-link>
+            <x-responsive-nav-link class="{{ (request()->segment(1) == 'sektor') ? 'active' : '' }}" :active="0" href="{{url('/sektor')}}">Sektor</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Sintua</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Sektor</x-responsive-nav-link>
           </div>
         </div>
       </div>
+
+      <div class="relative" x-data="{ open: true }">
+        <button @click="open = !open" class="text-right flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-gray-500 text-white rounded-md dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+          <span class="float-left font-bold text-lg">Data Transaksi</span>
+          <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
+        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="w-full mt-2 origin-top-right rounded-md shadow-lg">
+          <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Baptis</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Sidi</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Jemaat Baru</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Jemaat Lahir</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Pernikahan</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Meninggal</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Pindah</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Katekisasi</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Ikatan Janji</x-responsive-nav-link>
+            <x-responsive-nav-link class="" :active="0" href="{{url('#')}}">Perjanjian dan Permohonan Menikah</x-responsive-nav-link>
+          </div>
+        </div>
+      </div>
+
+      <div class="relative">
+        <button class="text-right flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-gray-500 text-white rounded-md dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+          <span class="float-left font-bold text-lg">Laporan</span>
+        </button>
+      </div>
+      <br/>
+      <br/>
+
     </nav>
     <x-popup-confirm
       :modalId="'modal-logout'"
@@ -49,4 +88,11 @@
       :actionUrl="url('logout')"
       :actionMethod="'POST'"
     />
+
+    <style>
+       nav a.active{
+        background-color: black;
+        color: white;
+      }
+    </style>
   </div>
