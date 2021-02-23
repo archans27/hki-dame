4d7200faffd1fb9b39658abfcaf2b825da7e44c4
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-02-23 23:44:47 +0700

    Create Sintua backbone

---
+++ app/Http/Controllers/SintuaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sintua;
+use Illuminate\Http\Request;
+
+class SintuaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Sintua  $sintua
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sintua $sintua)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Sintua  $sintua
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sintua $sintua)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Sintua  $sintua
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sintua $sintua)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Sintua  $sintua
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sintua $sintua)
+    {
+        //
+    }
+}
---
+++ app/Models/Sintua.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sintua extends Model
+{
+    use HasFactory;
+}
