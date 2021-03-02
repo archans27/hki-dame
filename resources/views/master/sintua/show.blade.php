5605d23a6bd944fd91fef1a89c8bfcd6b254276a
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-03-02 22:02:36 +0700

    create view edit and show

--- resources/views/master/sintua/show.blade.php
+++ resources/views/master/sintua/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Data Sektor`') }}
+            {{ __('Data Sintua`') }}
         </h2>
     </x-slot>
 
@@ -10,8 +10,8 @@
     <div class="py-12" style="clear: both">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" >
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5">
-                <x-back-button :class="'float-right'" :link="url('/sektor')"/>
-                <form action="{{url('/sektor/'.$sektor->id.'/edit')}}" class="float-right">
+                <x-back-button :class="'float-right'" :link="url('/sintua')"/>
+                <form action="{{url('/sintua/'.$sintua->id.'/edit')}}" class="float-right">
                     <button type="submit" class='relative bg-blue-500 text-white border border-blue-500 p-1 px-3 m-1 rounded overflow-hidden'>
                         <span class="material-icons">
                             mode_edit
@@ -21,8 +21,8 @@
                 </form>
                 <div class="clear-both"></div>
 
-                <p class="text-md font-bold text-blue-500">Nama Sektor</p><p>{{$sektor->nama}}</p><br/>
-                <p class="text-md font-bold text-blue-500">Wilayah Sektor</p><p>{{$sektor->wilayah}}</p><br/>
+                <p class="text-md font-bold text-blue-500">Nama Sintua</p><p>{{$sintua->nama}}</p><br/>
+                <p class="text-md font-bold text-blue-500">Sektor</p><p>{{$sintua->nama_sektor}}</p><br/>
             </div>
         </div>
     </div>
