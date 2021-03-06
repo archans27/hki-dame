cc7a000a80edea62934e5af9f3e0ba0989a7680f
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-03-06 17:04:19 +0700

    Create roles for each user admin||super

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,72 +19,86 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Admin HKI',
             'email' => 'hkidame@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'super'
         ]);
         User::create([
             'name' => 'Admin Sektor01',
             'email' => 'sektor1@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor02',
             'email' => 'sektor2@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor03',
             'email' => 'sektor3@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor04',
             'email' => 'sektor4@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor05',
             'email' => 'sektor5@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor06',
             'email' => 'sektor6@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor07',
             'email' => 'sektor7@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor08',
             'email' => 'sektor8@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor09',
             'email' => 'sektor9@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor10',
             'email' => 'sektor10@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor11',
             'email' => 'sektor11@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor12',
             'email' => 'sektor12@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
         User::create([
             'name' => 'Admin Sektor13',
             'email' => 'sektor13@mail.com',
-            'password' => Hash::make('password')
+            'password' => Hash::make('password'),
+            'role' => 'admin'
         ]);
     }
 }
