3cedb5bfedb6d3c3de2512c42b188ce6aa373f00
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-03-09 21:10:39 +0700

    Buat routes untuk ganti kepala keluarga

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', function () {return view('welcome');});
+Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth'])->name('dashboard');
+Route::post('/gantiKepalaKeluarga', [\App\Http\Controllers\KeluargaController::class, 'gantiKepalaKeluarga']);
 
 Route::group(['middleware' => 'auth'], function() {
     Route::resources([
