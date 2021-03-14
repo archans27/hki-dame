005e0d3a3ca0aa65309b29d863a209d8ed061b66
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-03-14 09:03:38 +0700

    Remake Jemaat baru controller due ucapanSyukur change

--- app/Http/Controllers/JemaatBaruController.php
+++ app/Http/Controllers/JemaatBaruController.php
@@ -53,17 +53,64 @@ class JemaatBaruController extends Controller
         $jemaat = $jemaat->fill($validated);
         $jemaat->save();
 
-        $ucapanSyukur = UcapanSyukur::create([
-            "id" => $uuid['ucapanSyukurId'],
-            "acara" => 'jemaatBaru',
-            "record" => $uuid['jemaatBaruId'],
-            "tk_gereja" => $validated['tk_gereja'],
-            "tk_pendeta" => $validated['tk_pendeta'],
-            "tk_majelis" => $validated['tk_majelis'],
-            "tk_guru_huria" => $validated['tk_guru_huria'],
-            "tk_pengembangan" => $validated['tk_pengembangan'],
-            "tanggal" => $validated['tanggal_anggota']
-        ]);
+        /**
+         * One line official Laravel style
+         */
+        // $ucapanSyukur = UcapanSyukur::create([
+        //     "id" => $uuid['ucapanSyukurId'],
+        //     "acara" => 'jemaatBaru',
+        //     "record" => $uuid['jemaatBaruId'],
+        //     "tk_gereja" => $validated['tk_gereja'],
+        //     "tk_pendeta" => $validated['tk_pendeta'],
+        //     "tk_majelis" => $validated['tk_majelis'],
+        //     "tk_guru_huria" => $validated['tk_guru_huria'],
+        //     "tk_pengembangan" => $validated['tk_pengembangan'],
+        //     "tanggal" => $validated['tanggal_anggota']
+        // ]);
+
+        //TODO Project Lead want (array style)
+        $tk_gereja = new UcapanSyukur();
+        $tk_gereja->ucapan_syukur_id = $uuid['ucapanSyukurId'];
+        $tk_gereja->untuk = "gereja";
+        $tk_gereja->besaran = $validated['tk_gereja'];
+        $tk_gereja->dari_acara = "jemaatBaru";
+        $tk_gereja->record = $uuid['jemaatBaruId'];
+        $tk_gereja->tanggal = $validated['tanggal_anggota'];
+        $tk_gereja->save();
+        $tk_pendeta = new UcapanSyukur();
+        $tk_pendeta->ucapan_syukur_id = $uuid['ucapanSyukurId'];
+        $tk_pendeta->untuk = "pendeta";
+        $tk_pendeta->besaran = $validated['tk_pendeta'];
+        $tk_pendeta->dari_acara = "jemaatBaru";
+        $tk_pendeta->record = $uuid['jemaatBaruId'];
+        $tk_pendeta->tanggal = $validated['tanggal_anggota'];
+        $tk_pendeta->save();
+        $tk_majelis = new UcapanSyukur();
+        $tk_majelis->ucapan_syukur_id = $uuid['ucapanSyukurId'];
+        $tk_majelis->untuk = "majelis";
+        $tk_majelis->besaran = $validated['tk_majelis'];
+        $tk_majelis->dari_acara = "jemaatBaru";
+        $tk_majelis->record = $uuid['jemaatBaruId'];
+        $tk_majelis->tanggal = $validated['tanggal_anggota'];
+        $tk_majelis->save();
+        $tk_guru_huria = new UcapanSyukur();
+        $tk_guru_huria->ucapan_syukur_id = $uuid['ucapanSyukurId'];
+        $tk_guru_huria->untuk = "guru_huria";
+        $tk_guru_huria->besaran = $validated['tk_guru_huria'];
+        $tk_guru_huria->dari_acara = "jemaatBaru";
+        $tk_guru_huria->record = $uuid['jemaatBaruId'];
+        $tk_guru_huria->tanggal = $validated['tanggal_anggota'];
+        $tk_guru_huria->save();
+        $tk_pengembangan = new UcapanSyukur();
+        $tk_pengembangan->ucapan_syukur_id = $uuid['ucapanSyukurId'];
+        $tk_pengembangan->untuk = "pengembangan";
+        $tk_pengembangan->besaran = $validated['tk_pengembangan'];
+        $tk_pengembangan->dari_acara = "jemaatBaru";
+        $tk_pengembangan->record = $uuid['jemaatBaruId'];
+        $tk_pengembangan->tanggal = $validated['tanggal_anggota'];
+        $tk_pengembangan->save();
+
+        
 
         $jemaatBaru = JemaatBaru::create([
             "id" => $uuid['jemaatBaruId'],
@@ -84,16 +131,33 @@ class JemaatBaruController extends Controller
     public function show($id)
     {
         $jemaatBaru = JemaatBaru::customGet($id);
-        return view('transaksi.jemaatBaru.show', [ 'jemaatBaru' => $jemaatBaru ]);
+        $ucapanSyukurs = UcapanSyukur::where('ucapan_syukur_id', '=', $jemaatBaru->ucapan_syukur_id)->get();
+        $ucapanSyukurToArray = array();
+        foreach($ucapanSyukurs as $ucapanSyukur)
+        {
+            $ucapanSyukurToArray[$ucapanSyukur->untuk] = $ucapanSyukur->besaran;
+        }
+        return view('transaksi.jemaatBaru.show', [
+            'jemaatBaru' => $jemaatBaru,
+            'ucapanSyukur' => $ucapanSyukurToArray
+        ]);
     }
 
 
     public function edit($id, Sektor $sektor)
     {
         $jemaatBaru = JemaatBaru::customGet($id);
+        $ucapanSyukurs = UcapanSyukur::where('ucapan_syukur_id', '=', $jemaatBaru->ucapan_syukur_id)->get();
+        $ucapanSyukurToArray = array();
+        foreach($ucapanSyukurs as $ucapanSyukur)
+        {
+            $ucapanSyukurToArray[$ucapanSyukur->untuk] = $ucapanSyukur->besaran;
+        }
+        //dd($ucapanSyukurToArray);   
         return view('transaksi.jemaatBaru.edit', [
             'jemaat' => $jemaatBaru,
-            'sektors' => $sektor->all()
+            'ucapanSyukur' => $ucapanSyukurToArray,
+            'sektors' => $sektor->all(),
         ]);
     }
 
@@ -105,8 +169,37 @@ class JemaatBaruController extends Controller
         $jemaatBaru->save();
         $jemaat = Jemaat::withoutGlobalScope('temporary')->find($jemaatBaru->jemaat_id)->fill($validated);
         $jemaat->save();
-        $ucapanSyukur = UcapanSyukur::find($jemaatBaru->ucapan_syukur_id)->fill($validated);
-        $ucapanSyukur->save();
+
+        $tk_gereja = UcapanSyukur::where('ucapan_syukur_id','=',$jemaatBaru->ucapan_syukur_id)
+            ->where('untuk', '=', 'gereja')
+            ->first()
+        ;
+        $tk_gereja->besaran = $validated['tk_gereja'];
+        $tk_gereja->save();
+        $tk_pendeta = UcapanSyukur::where('ucapan_syukur_id','=',$jemaatBaru->ucapan_syukur_id)
+            ->where('untuk', '=', 'pendeta')
+            ->first()
+        ;
+        $tk_pendeta->besaran = $validated['tk_pendeta'];
+        $tk_pendeta->save();
+        $tk_majelis = UcapanSyukur::where('ucapan_syukur_id','=',$jemaatBaru->ucapan_syukur_id)
+            ->where('untuk', '=', 'majelis')
+            ->first()
+        ;
+        $tk_majelis->besaran = $validated['tk_majelis'];
+        $tk_majelis->save();
+        $tk_guru_huria = UcapanSyukur::where('ucapan_syukur_id','=',$jemaatBaru->ucapan_syukur_id)
+            ->where('untuk', '=', 'guru_huria')
+            ->first()
+        ;
+        $tk_guru_huria->besaran = $validated['tk_guru_huria'];
+        $tk_guru_huria->save();
+        $tk_pengembangan = UcapanSyukur::where('ucapan_syukur_id','=',$jemaatBaru->ucapan_syukur_id)
+            ->where('untuk', '=', 'pengembangan')
+            ->first()
+        ;
+        $tk_pengembangan->besaran = $validated['tk_pengembangan'];
+        $tk_pengembangan->save();
 
         return redirect("/jemaatBaru/$id")
             ->with('succeed', "Perubahan Jemaat baru dengan nama ".$jemaat->nama." sudah tersimpan ke database")
