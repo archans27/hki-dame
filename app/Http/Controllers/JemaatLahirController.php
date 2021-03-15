b5566b8cc32394f085c267b65c6383078e0e9bca
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-03-15 20:23:47 +0700

    create jemaat lahir

---
+++ app/Http/Controllers/JemaatLahirController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\jemaat_lahir;
+use Illuminate\Http\Request;
+
+class JemaatLahirController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\jemaat_lahir  $jemaat_lahir
+     * @return \Illuminate\Http\Response
+     */
+    public function show(jemaat_lahir $jemaat_lahir)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\jemaat_lahir  $jemaat_lahir
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(jemaat_lahir $jemaat_lahir)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\jemaat_lahir  $jemaat_lahir
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, jemaat_lahir $jemaat_lahir)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\jemaat_lahir  $jemaat_lahir
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(jemaat_lahir $jemaat_lahir)
+    {
+        //
+    }
+}
