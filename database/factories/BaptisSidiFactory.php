1f96695b5b0cb5336a6f200f6d53fa24aed8965f
Daniel Christianto Widodo <danielchristianto77@gmail.com>
2021-03-28 13:34:13 +0700

    Factory and Seeder BaptisSidi

---
+++ database/factories/BaptisSidiFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BaptisSidi;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BaptisSidiFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = BaptisSidi::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
