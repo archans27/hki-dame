b59c492f783a08ef8bf6a83a346655eeb46f2c81
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2021-05-06 11:03:27 +0700

    test push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>
-      HKI Dame - Bandung
+      HKI Dame - Bandung 
     </title>
     <meta name="description" content="" />
     <meta name="keywords" content="" />
