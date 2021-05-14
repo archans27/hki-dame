9a6b7efb282fcba90764660573b24c28b8af0a4d
dinoyosuaa <dinoyosuapardede@rocketmail.com>
2021-05-14 15:08:32 +0700

    Enhancement Jemaat Baru

--- resources/views/components/form-jemaat-baru.blade.php
+++ resources/views/components/form-jemaat-baru.blade.php
@@ -3,7 +3,7 @@
     <legend class="px-2 text-lg">Data permohonan jemaat baru:</legend>
 
     <label for="lampiran" class="block text-black mt-3 font-bold">Melampiran Copy/Asli:</label>
-    <input type="text" name="lampiran" value="{{old('lampiran',$jemaat->lampiran  ?? '')}}" placeholder="Kartu Keluarga" class="rounded-md px-4 py-2 focus:outline-none bg-gray-100 lg:w-1/2 sm:w-full"/>
+    <input type="text" name="lampiran" value="{{old('lampiran',$jemaat->lampiran  ?? '')}}" placeholder="Surat Pindah, Surat Nikah, Akta Lahir, dll" class="rounded-md px-4 py-2 focus:outline-none bg-gray-100 lg:w-1/2 sm:w-full"/>
     @error('lampiran')
         <div class="text-red-500">{{ $message }}</div>
     @enderror
@@ -28,6 +28,7 @@
 
     <label for="persembahan_tahunan" class="block text-black mt-3 font-bold">Bersedia memberikan persembahan tahunan minimal:</label>
     <input type="text" name="persembahan_tahunan" value="{{old('persembahan_tahunan',$jemaat->persembahan_tahunan  ?? '')}}" placeholder="Persembahan tahunan minimal (numerik tanpa tanda)" class="rounded-md px-4 py-2  focus:outline-none bg-gray-100 lg:w-1/2 sm:w-full"/>
+    <label for="note" class="block text-black mt-1 italic" style="font-size: 12px; ">*Diperuntukkan untuk pengembangan gereja. Minimal Rp. 420.000,- (85% untuk diakonia, 15 % untuk Pembangunan)</label>
     @error('persembahan_tahunan')
         <div class="text-red-500">{{ $message }}</div>
     @enderror
